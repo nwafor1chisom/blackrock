9045edcf4a7d627cd407be7724fe2f12635741a0
Austine <nwaforaugustine820@gmail.com>
2026-05-04 00:49:37 +0100

    fix: add Vercel template path

--- blackrock/settings.py
+++ blackrock/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'blackrock.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates'],
+        'DIRS': [os.path.join(BASE_DIR, 'templates'), '/var/task/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
