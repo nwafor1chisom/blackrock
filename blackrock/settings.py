186c5beb9c581a2662b3aa133bc0a4f93051146f
Austine <nwaforaugustine820@gmail.com>
2026-05-04 01:15:04 +0100

    fix: update template dirs to include blackrock/templates

--- blackrock/settings.py
+++ blackrock/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'blackrock.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates'), '/var/task/templates'],
+        'DIRS': [BASE_DIR / 'templates', BASE_DIR / 'blackrock' / 'templates', '/var/task/blackrock/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
