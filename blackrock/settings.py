e4e86919269dfc719a7678a5642855767304dc0b
Austine <nwaforaugustine820@gmail.com>
2026-05-04 01:58:36 +0100

    fix: switch to PostgreSQL

--- blackrock/settings.py
+++ blackrock/settings.py
@@ -2,7 +2,7 @@ import os
 
 from pathlib import Path
 from decouple import Config, RepositoryEnv, AutoConfig
-
+import dj_database_url
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 env_file = os.path.join(BASE_DIR, ".env")
@@ -73,10 +73,10 @@ TEMPLATES = [
 WSGI_APPLICATION = 'blackrock.wsgi.application'
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=f'sqlite:///{BASE_DIR / "db.sqlite3"}',
+        conn_max_age=600
+    )
 }
 
 AUTH_PASSWORD_VALIDATORS = [
