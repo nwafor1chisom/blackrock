3d984a54735381b4e251a886410ed4b6fda00949
Austine <nwaforaugustine820@gmail.com>
2026-05-05 20:43:24 +0100

    Fix STATICFILES_STORAGE error

--- blackrock/settings.py
+++ blackrock/settings.py
@@ -116,9 +116,13 @@ USE_I18N = True
 USE_TZ = True
 
 
+
+
+
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'  # ← ADD THIS
 
 MEDIA_URL = '/media/'
 
