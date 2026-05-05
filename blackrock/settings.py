9083b54c49580ba32dc3743bd9c900e78616385a
Austine <nwaforaugustine820@gmail.com>
2026-05-05 21:03:17 +0100

    Remove duplicate staticfiles

--- blackrock/settings.py
+++ blackrock/settings.py
@@ -28,7 +28,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'cloudinary_storage',
     'django.contrib.staticfiles',
-    'django.contrib.staticfiles',
     'cloudinary',
     'django.contrib.humanize',
    
