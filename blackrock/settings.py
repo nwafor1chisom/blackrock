8cb967f2ebd54caf220f8b926138a5750bcf1e86
Austine <nwaforaugustine820@gmail.com>
2026-05-05 01:34:04 +0100

    Fix email host to SendGrid

--- blackrock/settings.py
+++ blackrock/settings.py
@@ -161,19 +161,14 @@ AUTH_USER_MODEL = 'users.User'
 # ─────────────────────────────────────────────
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-
 EMAIL_HOST = 'smtp.sendgrid.net'
 EMAIL_PORT = 587
-
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
-
 EMAIL_HOST_USER = 'apikey'
 EMAIL_HOST_PASSWORD = config('SENDGRID_API_KEY', default='')
-
 EMAIL_TIMEOUT = 30
 EMAIL_FAIL_SILENTLY = False
-
 DEFAULT_FROM_EMAIL = 'BlackRock Support <supportblackrock@gmail.com>'
 SERVER_EMAIL = DEFAULT_FROM_EMAIL
 
