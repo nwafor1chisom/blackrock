c708b280fe6ce6ce5e18f6d8707474a01d0b863c
Austine <nwaforaugustine820@gmail.com>
2026-05-06 14:33:18 +0100

    Remove decouple from Cloudinary config

--- blackrock/settings.py
+++ blackrock/settings.py
@@ -127,16 +127,17 @@ STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage' # 
 MEDIA_URL = '/media/'
 
 # Cloudinary for all environments  
+
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 CLOUDINARY_STORAGE = {
-    'CLOUD_NAME': os.environ.get('CLOUDINARY_CLOUD_NAME', config('CLOUDINARY_CLOUD_NAME')),
-    'API_KEY': os.environ.get('CLOUDINARY_API_KEY', config('CLOUDINARY_API_KEY')),
-    'API_SECRET': os.environ.get('CLOUDINARY_API_SECRET', config('CLOUDINARY_API_SECRET')),
+    'CLOUD_NAME': os.environ.get('CLOUDINARY_CLOUD_NAME'),
+    'API_KEY': os.environ.get('CLOUDINARY_API_KEY'),
+    'API_SECRET': os.environ.get('CLOUDINARY_API_SECRET'),
 }
 cloudinary.config(
-    cloud_name=os.environ.get('CLOUDINARY_CLOUD_NAME', config('CLOUDINARY_CLOUD_NAME')),
-    api_key=os.environ.get('CLOUDINARY_API_KEY', config('CLOUDINARY_API_KEY')),
-    api_secret=os.environ.get('CLOUDINARY_API_SECRET', config('CLOUDINARY_API_SECRET')),
+    cloud_name=os.environ.get('CLOUDINARY_CLOUD_NAME'),
+    api_key=os.environ.get('CLOUDINARY_API_KEY'),
+    api_secret=os.environ.get('CLOUDINARY_API_SECRET'),
 )
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
